4a4592fcac8018336672304334e349c793b56975
sendess <sandeshacha@gmail.com>
2022-08-06 18:39:15 +0545

    fully_developed model

---
+++ collab_bg_awake_fro_pc.py
@@ -0,0 +1,13 @@
+from pickle import TRUE
+import pyautogui
+import os
+
+
+for i in TRUE:
+    pyautogui.moveTo(200,200,duration= 5)
+
+    pyautogui.moveTo(200,1000,duration= 5)
+
+    pyautogui.moveTo(1000,200,duration= 5)
+
+    pyautogui.moveTo(1000,1000,duration= 5)
